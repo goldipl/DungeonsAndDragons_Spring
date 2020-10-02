e3a4f28d12f80f6bc6aba362413cab63bbd01ce7
goldipl <mgodlewski92@gmail.com>
2020-10-02 17:12:00 +0200

    first commit

---
+++ src/main/java/com/goldipl/DungeonAndDragonsApplication.java
@@ -0,0 +1,13 @@
+package com.goldipl;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DungeonAndDragonsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DungeonAndDragonsApplication.class, args);
+	}
+
+}
