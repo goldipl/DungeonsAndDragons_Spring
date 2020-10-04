578d1d1ab0288b20ce5c8c789a856983f28cd012
goldipl <mgodlewski92@gmail.com>
2020-10-04 22:00:30 +0200

    New tests to new methods: extraGold and ExtraWood added

--- src/test/java/com/goldipl/KnightTests.java
+++ src/test/java/com/goldipl/KnightTests.java
@@ -9,10 +9,37 @@ public class KnightTests {
     @Test
     public void martinezKnightTest() {
         Quest quest = new Quest("Save The King");
-        Knight knight = new Knight("Martinez", 30, 2000,2000, quest);
+        Knight knight = new Knight("Martinez", 30, 2000, 2000, quest);
         knight.setAge(32);
         Knight knightExcept = new Knight("Martinez", 32, 2000, 2000, quest);
         assertEquals(knight.toString(), knightExcept.toString());
+    }
 
+    @Test
+    public void martinezKnightSetExtraGoldTest() {
+        Quest quest = new Quest("Save The King");
+        Knight knight = new Knight("Martinez", 30, 2000, 2000, quest);
+        knight.setExtraGold(1000);
+        Knight knightExcept = new Knight("Martinez", 30, 3000, 2000, quest);
+        assertEquals(knight.toString(), knightExcept.toString());
+    }
+
+    @Test
+    public void martinezKnightSetExtraWoodTest() {
+        Quest quest = new Quest("Save The King");
+        Knight knight = new Knight("Martinez", 30, 2000, 2000, quest);
+        knight.setExtraWood(1400);
+        Knight knightExcept = new Knight("Martinez", 30, 2000, 3400, quest);
+        assertEquals(knight.toString(), knightExcept.toString());
+    }
+
+    @Test
+    public void martinezKnightSetExtraGoldANdWoodTest() {
+        Quest quest = new Quest("Save The King");
+        Knight knight = new Knight("Martinez", 30, 2000, 2000, quest);
+        knight.setExtraGold(1200);
+        knight.setExtraWood(1400);
+        Knight knightExcept = new Knight("Martinez", 30, 3200, 3400, quest);
+        assertEquals(knight.toString(), knightExcept.toString());
     }
 }
