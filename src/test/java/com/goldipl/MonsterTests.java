0909852ca935d9b0dfda5534eafb58a178d3ed45
goldipl <mgodlewski92@gmail.com>
2020-10-20 21:45:10 +0200

    monsters jUnit5 tests added

--- src/test/java/com/goldipl/MonsterTests.java
+++ src/test/java/com/goldipl/MonsterTests.java
@@ -1,6 +1,9 @@
 package com.goldipl;
 
 import com.goldipl.monsters.Dragon;
+import com.goldipl.monsters.DragonLord;
+import com.goldipl.monsters.Goblin;
+import com.goldipl.monsters.Witch;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
@@ -17,5 +20,43 @@ public class MonsterTests {
         assertEquals(dragonix.getHp(), 190);
         assertEquals(dragonix.getLvl(), 5);
         assertEquals(dragonix.getName(), "Dragonix");
+    }
+
+    @Test
+    public void marcoxGoblinTest() {
+        Goblin marcox = new Goblin("Marcox", 2, 100);
+        marcox.setHp(240);
+        marcox.setLvl(6);
+        Goblin goblinExcept = new Goblin("Marcox", 6, 240);
+        assertEquals(marcox.toString(), goblinExcept.toString());
+        assertEquals(marcox.getHp(), 240);
+        assertEquals(marcox.getLvl(), 6);
+        assertEquals(marcox.getName(), "Marcox");
+    }
+
+    @Test
+    public void fadraxWitchTest() {
+        Witch fadrax = new Witch("Fadrax", 12, 340);
+        fadrax.setHp(400);
+        fadrax.setLvl(9);
+        Witch witchExcept = new Witch("Fadrax", 9, 400);
+        assertEquals(fadrax.toString(), witchExcept.toString());
+        assertEquals(fadrax.getHp(), 400);
+        assertEquals(fadrax.getLvl(), 9);
+        assertEquals(fadrax.getName(), "Fadrax");
+    }
+
+    @Test
+    public void redonixDragonLordTest() {
+        DragonLord redonix = new DragonLord("Redonix", 50, 1200, 400);
+        redonix.setHp(2000);
+        redonix.setLvl(60);
+        redonix.setExtraPower(1000);
+        DragonLord dragonLordExcept = new DragonLord("Redonix", 60, 2000, 1000);
+        assertEquals(redonix.toString(), dragonLordExcept.toString());
+        assertEquals(redonix.getHp(), 2000);
+        assertEquals(redonix.getLvl(), 60);
+        assertEquals(redonix.getExtraPower(), 1000);
+        assertEquals(redonix.getName(), "Redonix");
     }
 }
